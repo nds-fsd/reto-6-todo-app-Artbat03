ed0dd00ea683380c857f0904f993184034d9116c
Artbat03 <artbat03@gmail.com>
2023-11-19 22:48:53 +0100

    [ FAIL ] Test Updating Todo

--- src/components/CardColorSelector/card-color-selector.jsx
+++ src/components/CardColorSelector/card-color-selector.jsx
@@ -1,33 +1,65 @@
-import { useEffect } from "react";
 import styles from "./card-color-selector.module.css";
+import { useForm } from "react-hook-form";
 
-export const CardColorSelector = ({ id }) => {
+export const CardColorSelector = ({ id, changeColorCard, onSumbit }) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors }
+  } = useForm();
+
   return (
-    <div className={styles.colorsContainer}>
+    <form className={styles.colorsContainer} {...register("color")}>
       <button
         className={`${styles.yellowBtn} ${styles.colorSelector}`}
-        onClick={() => console.log("yellow")}
+        value={"yellow"}
+        onClick={(event) => {
+          changeColorCard(event.target.value);
+          setValue("color", { color: event.target.value });
+          handleSubmit(onSumbit);
+        }}
       ></button>
       <button
         className={`${styles.greenBtn} ${styles.colorSelector}`}
-        onClick={() => console.log("green")}
+        onSubmit={handleSubmit(onSumbit)}
+        value={"green"}
+        onClick={(event) => {
+          changeColorCard(event.target.value);
+        }}
       ></button>
       <button
         className={`${styles.pinkBtn} ${styles.colorSelector}`}
-        onClick={() => console.log("pink")}
+        onSubmit={handleSubmit(onSumbit)}
+        value={"pink"}
+        onClick={(event) => {
+          changeColorCard(event.target.value);
+        }}
       ></button>
       <button
         className={`${styles.purpleBtn} ${styles.colorSelector}`}
-        onClick={() => console.log("purple")}
+        onSubmit={handleSubmit(onSumbit)}
+        value={"purple"}
+        onClick={(event) => {
+          changeColorCard(event.target.value);
+        }}
       ></button>
       <button
         className={`${styles.blueBtn} ${styles.colorSelector}`}
-        onClick={() => console.log("blue")}
+        onSubmit={handleSubmit(onSumbit)}
+        value={"blue"}
+        onClick={(event) => {
+          changeColorCard(event.target.value);
+        }}
       ></button>
       <button
         className={`${styles.greyBtn} ${styles.colorSelector}`}
-        onClick={() => console.log("grey")}
+        onSubmit={handleSubmit(onSumbit)}
+        value={"grey"}
+        onClick={(event) => {
+          changeColorCard(event.target.value);
+        }}
       ></button>
-    </div>
+    </form>
   );
 };
--- src/components/TodoCard/todo-card.jsx
+++ src/components/TodoCard/todo-card.jsx
@@ -1,33 +1,158 @@
+import { useForm } from "react-hook-form";
 import { CardColorSelector } from "../CardColorSelector/card-color-selector";
 import { RemoveTodo } from "../RemoveTodo/remove-todo";
 import styles from "./todo-card.module.css";
+import { useState } from "react";
 
-export const TodoCard = ({ id, text, done, setTodos, forceReload }) => {
-  return (
-    <div className={styles.cardContainer}>
-      <div className={`${styles.todoCard} ${styles.yellowCard}`}>
-        <input
-          type="checkbox"
-          className={styles.checkMark}
-          defaultChecked={done}
-        ></input>
-        <textarea
-          className={`${styles.todoText} ${styles.yellowCard}`}
-          defaultValue={text}
-        ></textarea>
+export const TodoCard = ({ id, text, color, done, setTodos, forceReload }) => {
+  const [editingModeActive, setEditingModeActive] = useState(false);
+  let [colorCard, setColorCard] = useState(color);
+  const [responseError, setResponseError] = useState("");
+  const [success, setSuccess] = useState(false);
 
-        <nav className={styles.navTodoContainer}>
-          <button className={styles.changeColorBtn}>
-            <img src="src/assets/TodoColor.svg" alt="Change Color" />
-          </button>
-          <button className={styles.changeColorBtn}>
-            <img src="src/assets/EditTodo.svg" alt="Edit" />
-          </button>
-          <RemoveTodo id={id} setTodos={setTodos} forceReload={forceReload} />
-        </nav>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
 
-        <CardColorSelector id={id} />
+  const handleClickEditTodo = () => {
+    return setEditingModeActive(!editingModeActive);
+  };
+
+  const onSumbit = (data) => {
+    fetch(`http://localhost:3001/todos/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          setResponseError(true);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSuccess(true);
+        setTodos(data);
+        forceReload();
+      })
+      .catch((error) => {
+        console.log(error);
+        setResponseError(true);
+      });
+  };
+
+  if (responseError) {
+    return (
+      <div>
+        <h1>ERROR</h1>
+        <h3>Unable to edit todo.</h3>
       </div>
-    </div>
+    );
+  }
+
+  if (success) {
+    return (
+      <div>
+        <h1>SUCCESS</h1>
+        <h3>Task edited successfully!</h3>
+      </div>
+    );
+  }
+
+  const classNames = {
+    yellow: styles.yellowCard,
+    green: styles.greenCard,
+    pink: styles.pinkCard,
+    purple: styles.purpleCard,
+    blue: styles.blueCard,
+    grey: styles.greyCard
+  };
+
+  const changeColorCard = (color) => {
+    console.log(color);
+    switch (color) {
+      case "yellow":
+        setColorCard(color);
+        classNames["yellow"];
+        break;
+      case "green":
+        setColorCard(color);
+        classNames["green"];
+        break;
+      case "pink":
+        setColorCard(color);
+        classNames["pink"];
+        break;
+      case "purple":
+        setColorCard(color);
+        classNames["purple"];
+        break;
+      case "blue":
+        setColorCard(color);
+        classNames["blue"];
+        break;
+      case "grey":
+        setColorCard(color);
+        classNames["grey"];
+        break;
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.cardContainer}>
+        <div className={`${styles.todoCard} ${classNames[colorCard]}`}>
+          <input
+            type="checkbox"
+            className={styles.checkMark}
+            defaultChecked={done}
+          ></input>
+          <textarea
+            className={`${styles.todoText} ${classNames[colorCard]}`}
+            defaultValue={text}
+          ></textarea>
+
+          <nav className={styles.navTodoContainer}>
+            <button className={styles.navTodoCardBtn}>
+              <img src="src/assets/TodoColor.svg" alt="Change Color" />
+            </button>
+            <button
+              className={styles.navTodoCardBtn}
+              onClick={handleClickEditTodo}
+            >
+              <img src="src/assets/EditTodo.svg" alt="Edit" />
+            </button>
+            <RemoveTodo id={id} changeColorCard={changeColorCard} />
+          </nav>
+
+          <CardColorSelector
+            id={id}
+            setColorCard={setColorCard}
+            onSumbit={onSumbit}
+          />
+        </div>
+      </div>
+      {editingModeActive && (
+        <div className={styles.editContainer}>
+          <form onSubmit={handleSubmit(onSumbit)}>
+            <label htmlFor="text">Text *:</label>
+            <input
+              type="text"
+              placeholder="Enter task text..."
+              {...register("text", { required: "Text is required." })}
+            />
+            {errors.text && (
+              <p className={styles.errorMessage}>{errors.text.message}</p>
+            )}
+
+            <button className={styles.createTodoBtn}>Edit Todo</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
